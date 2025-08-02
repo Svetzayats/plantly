d776d7fcfd3c9cd875ebd3e4019754ec7252c0c8
Svetlana <svetzayats@gmail.com>
2025-08-02 17:58:13 -0400

    🚧 Added switching between onboarding screen and tabs

---
+++ store/userStore.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+
+type UserStore = {
+  hasFinishedOnboarding: boolean;
+  toggleHadOnboarded: () => void;
+};
+
+export const useUserStore = create<UserStore>((set) => ({
+  hasFinishedOnboarding: false,
+  toggleHadOnboarded: () => {
+    set((state) => ({
+      ...state,
+      hasFinishedOnboarding: !state.hasFinishedOnboarding,
+    }));
+  },
+}));
