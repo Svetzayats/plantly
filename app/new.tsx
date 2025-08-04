5e063463ea6d954380bf2d25cadfe855f01bed2d
Svetlana <svetzayats@gmail.com>
2025-08-04 14:48:45 -0400

    ✨ Added page for plant

--- app/new.tsx
+++ app/new.tsx
@@ -50,7 +50,7 @@ export default function NewScreen() {
     if (Platform.OS === "web") return;
 
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: "images",
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
