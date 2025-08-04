66e3e30f00919c2ffd6df360d177689952ebf419
Svetlana <svetzayats@gmail.com>
2025-08-04 19:01:41 -0400

    💄 Added custom font

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { theme } from "@/theme";
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "expo-router";
@@ -54,8 +54,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   tagline: {
-    fontSize: 24,
+    fontSize: 34,
     color: theme.colorWhite,
     textAlign: "center",
+    fontFamily: Platform.select({
+      android: "Caveat_400Regular.ttf",
+      ios: "Caveat-Regular",
+    }),
   },
 });
